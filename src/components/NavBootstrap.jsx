0a75ad29aeeba5ecbd091f07ac782aebec42d66d
JuanRoa785 <roaporrasjuandiego@gmail.com>
2023-05-20 20:30:24 -0500

    Nav "responsive"

--- src/components/NavBootstrap.jsx
+++ src/components/NavBootstrap.jsx
@@ -4,43 +4,28 @@ import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import '../styles/navBootstrap.css'
+import icono from '../img/logo.png'
+import notification from '../img/campana-white.png'
+import userIcon from '../img/user-white.png'
+import buscador from '../img/lupa-white.png'
 
 function NavScrollExample() {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="transparent" variant="dark" expand="lg">
       <Container fluid>
-        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+      <img id='icono' src={icono} alt='icono'></img>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link>
+          <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll >
+              <Nav.Link href="#Home" id='link1'>Home</Nav.Link>
+              <Nav.Link href="#Help" id='link2'>Help</Nav.Link>
+              <Nav.Link href="#notifications" id='link3'><img src={notification} alt="campana blanca" id="CampanaIcon"/></Nav.Link>
+              <Nav.Link href="#User" id='link4'><img src={userIcon} alt="User Icon" id="userIcon"/></Nav.Link>
           </Nav>
           <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
+              <Form.Control type="search" placeholder="Search" className="me-2" aria-label="Search"/>
+              <Button variant="outline-secondary"><img src={buscador} alt="buscador" id="searcher"/></Button>
           </Form>
         </Navbar.Collapse>
       </Container>
