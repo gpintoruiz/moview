4a8c52e686fb860a998c2b0e37a6943f5f1fdb03
JuanRoa785 <roaporrasjuandiego@gmail.com>
2023-06-02 20:13:18 -0500

    Documentacion del codigo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,18 +11,22 @@ import { Link } from 'react-router-dom';
 function NavHeader() {
   return (
     <Navbar bg="transparent" variant="dark" expand="lg">
+      {/* Codigo para que el Navbar se colapse luego de superar un tamaño especifico */}
       <Container fluid>
       <Link to='/'>
+      {/* Icono del sitio web */}
         <img className='m-3 bg-transparent' id='icono' src={icono} alt='icono' style={{width:'120px'}}></img>
       </Link>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
+          {/* Codigo del nav que contiene el boton de home, help, notificaciones y el de Login */}
           <Nav className="ms-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll >
               <Button variant='outline-secondary' className='me-3' style={{border:'none',color:'white'}}>Home</Button>
               <Button variant='outline-secondary' className='me-3' style={{border:'none',color:'white'}}>Help</Button>
               <Notification/>
               <Login/>
           </Nav>
+          {/* Codigo del formulario usado para buscar una pelicula especifica */}
           <Form className="d-flex">
               <Form.Control type="search" placeholder="Search" className="me-2" aria-label="Search"/>
               <Button className='bi bi-search' variant="outline-secondary" style={{border:'none',color:'white'}}></Button>
