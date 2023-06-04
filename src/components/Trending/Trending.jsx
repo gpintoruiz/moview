94f24e9d41ba0d76f2dc8c780afe7793512b534a
JuanRoa785 <roaporrasjuandiego@gmail.com>
2023-06-04 11:42:19 -0500

    Minor change Carrusel

--- src/components/Trending/Trending.jsx
+++ src/components/Trending/Trending.jsx
@@ -63,7 +63,7 @@ function Trending() {
                   <b>{movie.title}</b>
                 </h3>
                 <p>
-                  <b>Estreno: {movie.release_date}</b>
+                  <b>{movie.release_date}</b>
                 </p>
                 <p>
                   <b>Generos: {movie.genre_ids}</b>
