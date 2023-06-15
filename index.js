e7ca71efbd117c40ae8ecf50b0cfdfb2f1ce4da6
Majinka10 <camachopinto_@hotmail.com>
2023-06-15 09:50:06 -0500

    Metiendo Middleware y Standard

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
 const express = require('express')
 const app = express()
+const logger = require('./loggerMiddleware')
 
 app.use(express.json())
 
+app.use(logger)
+
 let reseñas = [
-    {
-      "nombreResenador": "Juan Pérez",
-      "id": 1,
-      "resena": "Excelente película, muy recomendada",
-      "fecha": "2023-04-13"
-    },
-    {
-      "nombreResenador": "María Gómez",
-      "id": 2,
-      "resena": "La trama es interesante pero el final fue decepcionante",
-      "fecha": "2023-04-12"
-    },
-    {
-      "nombreResenador": "Pedro Rodríguez",
-      "id": 3,
-      "resena": "No me gustó, la historia es predecible",
-      "fecha": "2023-04-11"
-    }
-  ]
+  {
+    'nombreResenador': 'Juan Pérez',
+    'id': 1,
+    'resena': 'Excelente película, muy recomendada',
+    'fecha': '2023-04-13'
+  },
+  {
+    'nombreResenador': 'María Gómez',
+    'id': 2,
+    'resena': 'La trama es interesante pero el final fue decepcionante',
+    'fecha': '2023-04-12'
+  },
+  {
+    'nombreResenador': 'Pedro Rodríguez',
+    'id': 3,
+    'resena': 'No me gustó, la historia es predecible',
+    'fecha': '2023-04-11'
+  }
+]
 
 // const app = http.createServer((request, response) =>{
 //     response.writeHead(200,{'Content-Type':'application/json'})
@@ -30,21 +33,22 @@ let reseñas = [
 // })
 
 app.get('/', (request, response) =>{
-    response.send('<h1>Hellow World</h1>')
+  response.send('<h1>Hellow World</h1>')
 })
 
-app.get('/api/resenas',(request, response)=>{
-    response.json(reseñas)
+app.get('/api/resenas',(request, response, next)=>{
+  response.json(reseñas)
+  next()
 })
 
 app.get('/api/resenas/:id',(request, response)=>{
-    const id = request.params.id
-    const reseña = reseñas.find(reseña => reseña.id == id)
-    if (reseña){
-      response.json(reseña)
-    } else{
-      response.status(404).end()
-    }
+  const id = request.params.id
+  const reseña = reseñas.find(reseña => reseña.id == id)
+  if (reseña){
+    response.json(reseña)
+  } else{
+    response.status(404).end()
+  }
 })
 
 app.delete('/api/resenas/:id',(request, response)=>{
@@ -75,6 +79,12 @@ app.post('/api/resenas',(request, response)=>{
   reseñas = [... reseñas, newResena]
 
   response.status(201).json(newResena)
+})
+
+app.use((reques, response)=>{
+  response.status(404).json({
+    error:'Not found'
+  })
 })
 
 const PORT = 3001
