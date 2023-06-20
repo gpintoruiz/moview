1285f3746db47b04d018609e1c690df4d0ddf68b
Majinka10 <camachopinto_@hotmail.com>
2023-06-20 09:51:55 -0500

    Hola

--- src/components/Reseña/Reseña.jsx
+++ src/components/Reseña/Reseña.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Row, Col} from 'react-bootstrap'
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Reseña() {
     return (
